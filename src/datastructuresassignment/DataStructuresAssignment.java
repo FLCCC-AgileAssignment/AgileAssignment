26272b30ba943d14eb78306f3e65e98bb90d130d
taruc <taruc@PC07-KL>
2018-11-14 08:37:02 +0800

    commit

--- src/datastructuresassignment/DataStructuresAssignment.java
+++ src/datastructuresassignment/DataStructuresAssignment.java
@@ -21,6 +21,7 @@ public class DataStructuresAssignment {
         System.out.print("Hello!");
         System.out.print("Hello!");
         System.out.print("Hello!");
+        System.out.print("Hello!");
     }
     
 }
