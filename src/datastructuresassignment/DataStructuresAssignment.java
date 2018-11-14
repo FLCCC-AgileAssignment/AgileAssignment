483d75a0f478fccdc9f2db15c601b70887c1ccac
taruc <taruc@PC07-KL>
2018-11-14 08:25:18 +0800

    1st commit

---
+++ src/datastructuresassignment/DataStructuresAssignment.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructuresassignment;
+
+/**
+ *
+ * @author taruc
+ */
+public class DataStructuresAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Testing
+        
+        System.out.print("Hello!");
+        System.out.print("Hello!");
+        System.out.print("Hello!");
+    }
+    
+}
