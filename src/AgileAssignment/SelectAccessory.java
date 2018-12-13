79426f107a28b7b1edbd4d8972904de319b60245
Jian <Jian@user>
2018-12-13 14:08:59 +0800

    Commit

--- src/AgileAssignment/SelectAccessory.java
+++ src/AgileAssignment/SelectAccessory.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dsaassignment;
+package AgileAssignment;
 
 import java.awt.Image;
 import java.awt.Toolkit;
